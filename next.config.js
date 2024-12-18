25269881d5228f0fa49ad9d2725285d07bab3572
Rohan Ahmed <rohan.ahmed7887@gmail.com>
2024-12-19 02:06:37 +0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -4,8 +4,9 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  basePath: '',
-  assetPrefix: ''
+  basePath: process.env.NODE_ENV === 'production' ? '/mediclinic-app' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/mediclinic-app/' : '',
+  trailingSlash: true
 }
 
 module.exports = nextConfig
